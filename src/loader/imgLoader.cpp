896a36154507ce4aaaf2e1dfbe097a432dbe3900
Roharui <jack555586@gmail.com>
2022-08-24 16:49:02 +0900

    feat: 이미지 로더에 json 형식 추가

--- src/loader/imgLoader.cpp
+++ src/loader/imgLoader.cpp
@@ -4,13 +4,18 @@
 #include <map>
 #include <string>
 #include <vector>
+
+#include <fstream>
 #include <filesystem>
+
+#include <json/json.h>
 
 #include "utils/path.hpp"
 
 using std::map;
 using std::string;
-using std::vector;
+
+using namespace Json;
 
 namespace fs = std::filesystem;
 
@@ -18,37 +23,69 @@ class Loader
 {
 private:
   map<string, Texture2D> mapset;
+  Value openJSON(string path);
 
 public:
   Loader();
   Texture2D getTexture(string path);
 };
 
+// === private ===
+
+Value Loader::openJSON(string path)
+{
+  std::ifstream json_file;
+  json_file.open(path, std::ifstream::in | std::ifstream::binary);
+  Value v;
+  json_file >> v;
+  json_file.close();
+
+  return v;
+}
+
+// ==== public ====
+
 Loader::Loader()
 {
-  string path = "/resources";
-  string p = ExePath() + path;
-  for (auto &filename : fs::directory_iterator(p))
-  {
-    string file_path = filename.path().generic_string();
-    string base_filename = filename.path().stem().generic_string();
 
-    this->mapset[base_filename] = LoadTexture(file_path.c_str());
+  string resources_path = ExePath() + "/resources";
+  string img_json = "/.img.json";
+  string json_path = resources_path + img_json;
+
+  Value v = this->openJSON(json_path);
+
+  for (auto const &id : v.getMemberNames())
+  {
+    Value vid = v[id];
+    string img_path = resources_path + vid["filename"].asString();
+
+    if (vid.isMember("size"))
+    {
+      int size_x = v[id]["size"][0].asInt();
+      int size_y = v[id]["size"][1].asInt();
+      Image img = LoadImage(img_path.c_str());
+
+      ImageResize(&img, size_x, size_y);
+
+      Texture2D text2d = LoadTextureFromImage(img);
+      UnloadImage(img);
+
+      this->mapset[id] = text2d;
+    }
+    else
+    {
+      this->mapset[id] = LoadTexture(img_path.c_str());
+    }
   }
 }
 
 Texture2D Loader::getTexture(string path)
 {
 
-  // for (auto it : this->mapset)
-  // {
-  //   if (it.first == path)
-  //     return it.second;
-  // }
-  // return {0, 0, 0, 0, 0};
-  if (this->mapset.contains(path))
+  for (auto it : this->mapset)
   {
-    return this->mapset[path];
+    if (it.first == path)
+      return it.second;
   }
   return {0, 0, 0, 0, 0};
 }
